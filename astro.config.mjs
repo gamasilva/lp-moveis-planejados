7334b0bc9cf55ca3338a60c42af65de0e6dac43a
gamasilva <dan.syler@gmail.com>
2026-04-10 18:21:43 -0400

    versaofinal1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://moveisplanejadosluxo.com.br',
   build: {
     inlineStylesheets: 'always'
   },
   vite: {
     plugins: [tailwindcss()]
   },
+  integrations: [sitemap(), robotsTxt()],
   image: {
     domains: ['images.unsplash.com', 'i.ibb.co']
   }
